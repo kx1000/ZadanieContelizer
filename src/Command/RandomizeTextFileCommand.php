30137476e896a3a57feaa5097793cc4c7612f8e0
Kacper <kacper.rogula@gmail.com>
2021-04-10 09:45:47 +0200

    Obsługa polskich znaków

--- src/Command/RandomizeTextFileCommand.php
+++ src/Command/RandomizeTextFileCommand.php
@@ -8,24 +8,22 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 /**
- * php bin/console randomify:text-file
+ * php bin/console randomize:text-file
  */
-class RandomifyTextFileCommand extends Command
+class RandomizeTextFileCommand extends Command
 {
     const INPUT_FILE = 'src/Command/txts/input.txt';
     const OUTPUT_FILE = 'src/Command/txts/output.txt';
-    protected static $defaultName = 'randomify:text-file';
+
+    protected static $defaultName = 'randomize:text-file';
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
         $inputFileContent = file_get_contents(self::INPUT_FILE);
-
-        $randomifiedContent = preg_replace_callback("/[a-zA-Ząćęłńóśźż]*/", function(array $word) {
+        $randomifiedContent = preg_replace_callback("/[a-zA-Ząćęłńóśźż]+/ui", function(array $word) {
             return $this->randomifyWord($word[0]);
         }, $inputFileContent);
-
-        dump($randomifiedContent);
 
         file_put_contents(self::OUTPUT_FILE, $randomifiedContent);
 
@@ -36,16 +34,29 @@ class RandomifyTextFileCommand extends Command
 
     private function randomifyWord(string $word): string
     {
-        $chars = str_split($word);
+        $countChars = mb_strlen($word);
 
-        if (3 >= count($chars)) { // trzyliterowy wyraz również nie zostanie wymieszany
+        if (3 >= $countChars) { // trzyliterowy wyraz również nie zostanie wymieszany
             return $word;
         }
 
+        $chars = mb_str_split($word);
         $startChar = array_shift($chars);
         $endChar = array_pop($chars);
-        shuffle($chars);
 
-        return $startChar . implode($chars) . $endChar;
+        return $startChar . $this->mbStrShuffle(implode($chars)) . $endChar;
+    }
+
+    /**
+     * Miesza znaki w stringu
+     *
+     * str_shuffle i shuffle psuje kodowanie, brakuje funkcji obsługjącej znaki Multibyte
+     */
+    private function mbStrShuffle(string $str): string
+    {
+        preg_match_all('/./u', $str, $temp);
+        shuffle($temp[0]);
+        $str = join("", $temp[0]);
+        return $str;
     }
 }
