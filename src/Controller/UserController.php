aa27874b3a98d5eec1d57414ff25c1dbfb592106
Kacper <kacper.rogula@gmail.com>
2021-04-10 22:02:34 +0200

    Wyświetlanie i edycja użytkowników

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ApiCrudController extends Controller
+class UserController extends Controller
 {
     /**
-     * @Route("/api-crud", name="api_crud")
+     * @Route("/user", name="user_index")
      */
     public function index(): Response
     {
-        return $this->render('api_crud/index.html.twig');
+        return $this->render('user/index.html.twig');
     }
 }
